0eb9eacf293993f54d5c516f0fe8cbc4be4a7b78
mattkim17 <68535436+mattkim17@users.noreply.github.com>
2021-01-06 20:32:35 +0900

    Delete Question2.sql

--- Question2.sql
+++
@@ -1,8 +0,0 @@
--- Question 2
-
-SELECT employees.first_name, employees.last_name, employees.title, hire_date, COUNT(employees.first_name) as num
-FROM employees 
-
-INNER JOIN orders ON orders.employee_id = employees.employee_id 
-GROUP BY employees.first_name, employees.last_name, employees.title, hire_date
-ORDER BY COUNT(employees.first_name) DESC
